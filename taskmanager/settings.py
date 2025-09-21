f77ff1a083c7b0b500191792e8b709008710b29c
Shikhardeep Meena <shikhar4090@gmail.com>
2025-09-21 10:24:32 +0530

    added dotenv

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 

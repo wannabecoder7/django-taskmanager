29ee2f3b81b68df864025ef0a2c94ad2744f1fb0
Shikhardeep Meena <shikhar4090@gmail.com>
2025-09-21 09:41:31 +0530

    asgi,wsgi

--- taskmanager/wsgi.py
+++ taskmanager/wsgi.py
@@ -1,6 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanager.settings')
 
 application = get_wsgi_application()

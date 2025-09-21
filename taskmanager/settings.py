be6acc7383c920c917d02674103c3fff86d56f50
Shikhardeep Meena <shikhar4090@gmail.com>
2025-09-21 09:25:43 +0530

    secret key edit

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key-change-me")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
 
